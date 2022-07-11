cec4fefebde64276b96b6cc9dda37393cb4a55e9
Ari Falkner <ari@open.store>
2022-07-11 17:54:38 -0400

    remove required dependencies versions so that Ocean and AWS can be pinned elsewhere

--- versions.tf
+++ versions.tf
@@ -1,14 +1,3 @@
 terraform {
   required_version = ">= 0.13.1"
-
-  required_providers {
-    spotinst = {
-      source  = "spotinst/spotinst"
-      version = "~> 1.64.1"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.70.0"
-    }
-  }
 }
